24600fb4942cf4b518ca985daf82ad97b0903eb5
Vasanth Developer <vasanth@vasanthdeveloper.com>
2020-03-28 18:27:46 +0530

    💣 Big Bang!

---
+++ src/sandesha.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+//   ___    __________   |  Vasanth Developer (Vasanth Srivatsa)
+//   __ |  / /___  __ \  |  ------------------------------------------------
+//   __ | / / __  / / /  |  https://github.com/vasanthdeveloper/sandesha.git
+//   __ |/ /  _  /_/ /   |
+//   _____/   /_____/    |  Entryfile for sandesha project
+//                       |
+
+async function main(): Promise<void> {
+    console.log('Hello World!')
+}
+
+main()
